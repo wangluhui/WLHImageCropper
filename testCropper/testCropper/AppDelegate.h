64d74e72ab2bcd62d844659c78d21fc6e6ed4594
wangluhui <183049213@qq.com>
2016-03-06 17:42:38 +0800

    改进，对6s,ios9

---
+++ testCropper/testCropper/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testCropper
+//
+//  Created by wanglh on 16/3/6.
+//  Copyright © 2016年 wanglh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
